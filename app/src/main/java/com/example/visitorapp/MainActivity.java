f3877c068caae365d13aca1adb4f3031f9c1c6f3
nandanapk <nandanapk5566@gmail.com>
2024-12-19 11:48:25 +0530

    View all feature is added

--- app/src/main/java/com/example/visitorapp/MainActivity.java
+++ app/src/main/java/com/example/visitorapp/MainActivity.java
@@ -12,18 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         b1=(Button) findViewById(R.id.av);
+        b2=(Button) findViewById(R.id.va);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent o=new Intent(getApplicationContext(), AddActivity.class);
                 startActivity(o);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent o1=new Intent(getApplicationContext(), ViewAllVisitors.class);
+                startActivity(o1);
             }
         });
 
